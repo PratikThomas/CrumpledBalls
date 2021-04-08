85c4ef486f08f97badc842e353669526f6014328
PratikThomas <73641460+PratikThomas@users.noreply.github.com>
2021-04-08 14:34:05 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,56 +1,62 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-var groundSprite, ground;
+const Render = Matter.Render;
+var dustbinObj, paperObject,groundObject;
+var world;
 var Box1, Box2, Box3;
-var paper, paperSprite;
+
 
 function setup() {
-	createCanvas(1500, 700);
+	createCanvas(1600, 700);
+	rectMode(CENTER);
 
-	paperSprite=createSprite(20, 80, 35, 35);
-	//paper.shapeColor = color(0);
-
-	groundSprite=createSprite(width/2, height-35, width,10);
-	groundSprite.shapeColor=color(255)
-
-	Box1 = createSprite(1300, height-45, 200, 10, {isStatic:true});
-	Box1.shapeColor = color("RED");
-
-	Box2 = createSprite(1200, height-90, 10, 100, {isStatic:true});
-	Box2.shapeColor = color("RED");
-
-	Box3 = createSprite(1400, height-90, 10, 100, {isStatic:true});
-	Box3.shapeColor = color("RED");
 
 	engine = Engine.create();
 	world = engine.world;
 
-	paper = Bodies.circle(50 , 645 , 5 , {restitution:0, isStatic:true, friction:0.5, density:1.2});
-	World.add(world, paper);
+	Box1 = createSprite(width-200, height-45, 200, 10, {isStatic:true, restitution:0, friction:0.5});
+	Box1.shapeColor = color("RED");
 
-	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
-	World.add(world, ground);
+	Box2 = createSprite(1300, height-90, 10, 100, {isStatic:true, restitution:0, friction:0.5});
+	Box2.shapeColor = color("RED");
+
+	Box3 = createSprite(1500, height-90, 10, 100, {isStatic:true, restitution:0, friction:0.5});
+	Box3.shapeColor = color("RED");
+
+	paperObject=new paper(200,450,30);
+	groundObject=new Ground(width/2,670,width,20);
+	//Create a Ground	
+
+	/*var render = Render.create({
+	  element: document.body,
+	  engine: engine,
+	  options: {
+	    width: 1200,
+	    height: 700,
+	    wireframes: false
+	  }
+	});*/
 
 	Engine.run(engine);
+	//Render.run(render);  
 }
 
 
 function draw() {
-  background("Black");
-  Engine.update(engine);
-  paperSprite.x= paper.position.x;
-  paperSprite.y= paper.position.y;
-  paper.velocityY = -12;
-  keyPressed();
-  drawSprites(); 
+  rectMode(CENTER);
+  background(0);
+  Box1.display();
+  Box2.display();
+  Box3.display();
+  paperObject.display();
+  groundObject.display(); 
+  drawSprites();
 }
 
-function keyPressed(){
-  if(keyCode === UP_ARROW){
-	  Matter.Body.setStatic(paper,false);
-	  Matter.Body.applyForce(paper.body, paper.body.position, {x:85, y:-85});
-  }
+function keyPressed() {
+  	if (keyCode === UP_ARROW) {
+    	Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
+	}
 }
